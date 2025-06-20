05ad4faad94d206e6d3bebd16c466f7fec942fe7
Berkan <berkan_mandaci@hotmail.com>
2025-06-20 23:05:23 +0300

    port issue test

--- PawMatch.Api/Program.cs
+++ PawMatch.Api/Program.cs
@@ -16,7 +16,7 @@ public class Program
     public static void Main(string[] args)
     {
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:80", "http://0.0.0.0:8080");
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 // Add services to the container.
 
